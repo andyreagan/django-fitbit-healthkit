520a42573b209843e68ebf0331cf0e7892b30f90
Andy Reagan <andy@andyreagan.com>
2024-04-03 15:31:12 -0400

    Add a helper method for the return of the helpers. Localize the urls to the functions that need them.

--- django_fitbit_healthkit/methods.py
+++ django_fitbit_healthkit/methods.py
@@ -1,20 +1,17 @@
 from datetime import date
 from typing import Dict
 
+from django.conf import settings
+
 from .models import FitbitUser
 
-DAILY_ACTIVITY_SUMMARY_URL = (
-    "https://api.fitbit.com/1/user/-/activities/date/{date}.json"
-)
-SLEEP_LOG_BY_DATE_URL = "https://api.fitbit.com/1.2/user/-/sleep/date/{date}.json"
-SLEEP_LOG_BY_DATE_RANGE_URL = (
-    "https://api.fitbit.com/1.2/user/-/sleep/date/{start_date}/{end_date}.json"
-)
-ACTIVITY_INTRADAY_BY_DATE_URL = "https://api.fitbit.com/1/user/-/activities/{activity}/date/{date}/1d/{interval}.json"
-ACTIVITY_TIMESERIES_BY_DATE_URL = (
-    "https://api.fitbit.com/1/user/-/activities/{resource}/date/{date}/{period}.json"
-)
-ACTIVITY_TIMESERIES_BY_DATE_RANGE_URL = "https://api.fitbit.com/1/user/-/activities/{resource}/date/{start_date}/{end_date}.json"
+
+def json_or_empty_dict(response, err) -> Dict:
+    if settings.DEBUG:
+        print(response, err)
+    if err is None:
+        return response.json()
+    return {}
 
 
 def daily_activity_summary(fitbitUser: FitbitUser, d: date) -> Dict:
@@ -22,12 +19,12 @@ def daily_activity_summary(fitbitUser: FitbitUser, d: date) -> Dict:
     ref: https://dev.fitbit.com/build/reference/web-api/activity/get-daily-activity-summary/
     format: /1/user/[user-id]/activities/date/[date].json
     """
-    url = DAILY_ACTIVITY_SUMMARY_URL.format(date=d.isoformat())
-    response, err = fitbitUser.make_request("get", url)
-    print(response, err)
-    if err is None:
-        return response.json()
-    return {}
+    return json_or_empty_dict(
+        fitbitUser.make_request(
+            "get",
+            f"https://api.fitbit.com/1/user/-/activities/date/{d.isoformat()}.json",
+        )
+    )
 
 
 def sleep_log_by_date(fitbitUser: FitbitUser, d: date) -> Dict:
@@ -35,12 +32,11 @@ def sleep_log_by_date(fitbitUser: FitbitUser, d: date) -> Dict:
     ref: https://dev.fitbit.com/build/reference/web-api/sleep/get-sleep-log-by-date/
     format: /1.2/user/[user-id]/sleep/date/[date].json
     """
-    url = SLEEP_LOG_BY_DATE_URL.format(date=d.isoformat())
-    response, err = fitbitUser.make_request("get", url)
-    print(response, err)
-    if err is None:
-        return response.json()
-    return {}
+    return json_or_empty_dict(
+        fitbitUser.make_request(
+            "get", f"https://api.fitbit.com/1.2/user/-/sleep/date/{d.isoformat()}.json"
+        )
+    )
 
 
 def sleep_log_by_date_range(
@@ -56,14 +52,12 @@ def sleep_log_by_date_range(
     if (end_date - start_date).days > 100:
         print("Date range is too long, won't try to fetch data.")
         return {}
-    url = SLEEP_LOG_BY_DATE_RANGE_URL.format(
-        start_date=start_date.isoformat(), end_date=end_date.isoformat()
+    return json_or_empty_dict(
+        fitbitUser.make_request(
+            "get",
+            f"https://api.fitbit.com/1.2/user/-/sleep/date/{start_date.isoformat()}/{end_date.isoformat()}.json",
+        )
     )
-    response, err = fitbitUser.make_request("get", url)
-    print(response, err)
-    if err is None:
-        return response.json()
-    return {}
 
 
 def activity_intraday_by_date(
@@ -83,15 +77,13 @@ def activity_intraday_by_date(
     if interval not in ["1min", "5min", "15min"]:
         print(f"Invalid interval {interval}, won't try to fetch data.")
         return {}
-    url = ACTIVITY_INTRADAY_BY_DATE_URL.format(
-        activity=activity, date=d, interval=interval
+
+    return json_or_empty_dict(
+        fitbitUser.make_request(
+            "get",
+            f"https://api.fitbit.com/1/user/-/activities/{activity}/date/{d.isoformat()}/1d/{interval}.json",
+        )
     )
-    print(url)
-    response, err = fitbitUser.make_request("get", url)
-    print(response, err)
-    if err is None:
-        return response.json()
-    return {}
 
 
 def activity_timeseries_by_date(
@@ -132,15 +124,12 @@ def activity_timeseries_by_date(
     if period not in ["1d", "7d", "30d", "1w", "1m", "3m", "6m", "1y"]:
         print(f"Invalid period {period}, won't try to fetch data.")
         return {}
-    url = ACTIVITY_TIMESERIES_BY_DATE_URL.format(
-        resource=resource, date=d, period=period
+    return json_or_empty_dict(
+        fitbitUser.make_request(
+            "get",
+            f"https://api.fitbit.com/1/user/-/activities/{resource}/date/{d.isoformat()}/{period}.json",
+        )
     )
-    print(url)
-    response, err = fitbitUser.make_request("get", url)
-    print(response, err)
-    if err is None:
-        return response.json()
-    return {}
 
 
 def activity_timeseries_by_date_range(
@@ -190,14 +179,9 @@ def activity_timeseries_by_date_range(
     if resource not in all_activity_resources + tracker_only_resources:
         print(f"Invalid resource {resource}, won't try to fetch data.")
         return {}
-    url = ACTIVITY_TIMESERIES_BY_DATE_RANGE_URL.format(
-        resource=resource,
-        start_date=start_date.isoformat(),
-        end_date=end_date.isoformat(),
+    return json_or_empty_dict(
+        fitbitUser.make_request(
+            "get",
+            f"https://api.fitbit.com/1/user/-/activities/{resource}/date/{start_date.isoformat()}/{end_date.isoformat()}.json",
+        )
     )
-    print(url)
-    response, err = fitbitUser.make_request("get", url)
-    print(response, err)
-    if err is None:
-        return response.json()
-    return {}

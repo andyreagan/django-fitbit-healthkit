92d7a087324063137459993bbd3dcd3f30b0e1a4
Andy Reagan <andy@andyreagan.com>
2024-08-05 15:12:41 -0400

    Fix tests.

--- tests/test_views.py
+++ tests/test_views.py
@@ -5,8 +5,29 @@ from django.urls import reverse
 # path("success", views.success, name="fitbitsuccess"),
 # path("webhook", views.fitbit_subscription, name="fitbitsubscription"),
 
+# class MyViewTest(TestCase):
+#     def test_my_view(self):
+#         # Make a request to the view
+#         response = self.client.get(reverse("fitbitlogin"))
+        
+#         # Now use the response object with assertRedirects
+#         
+ 
+from urllib.parse import urlparse
+
 class MyViewTest(TestCase):
     def test_my_view(self):
-        response = self.client.get(reverse('fitbitlogin'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello, world!')
+        # Make a request to the view
+        response = self.client.get(reverse("fitbitlogin"))
+
+        # can't just do assertRedirects because the URL has query parameters
+        # self.assertRedirects(response, 'https://www.fitbit.com/oauth2/authorize', fetch_redirect_response=False)
+        
+        # Extract the location header
+        full_redirect_url = response['Location']
+        # Parse the URL to extract the scheme and netloc, ignoring query params
+        parsed_url = urlparse(full_redirect_url)
+        base_redirect_url = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
+        
+        # Compare the base URL without query parameters
+        self.assertEqual(base_redirect_url, 'https://www.fitbit.com/oauth2/authorize')
